b3c3664427350a1b5639f2ff79971fea995a899c
ShirishaGunta <shirishagunta3@gmail.com>
2025-11-13 21:12:07 +0530

    Initial commit

---
+++ drizzle.config.js
@@ -0,0 +1,10 @@
+import 'dotenv/config';
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  schema: './config/schema.js',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: "Add Your Own DB String"
+  },
+});
